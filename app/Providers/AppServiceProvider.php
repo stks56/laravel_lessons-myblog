f87345cdbf84e35e8c3eac5bac1726d6e41c5145
stks56 <t.suenaga428@gmail.com>
2019-02-18 21:24:35 +0900

    new file

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        if (\DB::getDriverName() === 'sqlite') {
+            \DB::statement(\DB::raw('PRAGMA forreign_keys=1'));
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
